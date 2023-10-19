51ded452bb1170fdac6b48395526fb95ea5bc817
MoatasemGamal <moatasemco@gmail.com>
2023-10-19 19:43:45 +0300

    implement simple view called index
    - Add bootstrap, favicon.ico into a public folder
    - Implement a index.view.php

---
+++ src/App/views/index.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Back</title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="text-bg-dark d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
+        <h1>Index View</h1>
+    </div>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
