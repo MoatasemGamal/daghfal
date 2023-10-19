22a2f1419026a93dba82126b1f1924b0fd3c26f9
MoatasemGamal <moatasemco@gmail.com>
2023-10-19 23:43:12 +0300

    register PreventGuestMiddleware to profile action

--- src/App/Controllers/UserController.php
+++ src/App/Controllers/UserController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Controllers;
+use App\Middleware\PreventGuestMiddleware;
 use Core\Bases\BaseController;
 
 
 class UserController extends BaseController{
+
+    public function __construct(){
+        $this->registerMiddleware(new PreventGuestMiddleware(['profile']));
+    }
+
     public function login()
     {
         return view("auth.login");
