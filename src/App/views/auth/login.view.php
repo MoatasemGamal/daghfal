a7ceaf2a173542df82dc5231f9f5c098c69bb4f5
MoatasemGamal <moatasemco@gmail.com>
2023-10-19 23:33:04 +0300

    just change first input name

--- src/App/views/auth/login.view.php
+++ src/App/views/auth/login.view.php
@@ -13,7 +13,7 @@
     <div class="text-bg-dark d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
         <form method="post" class="p-5 border rounded">
             <h1>Login</h1>
-            <input type="text" class="form-control mb-3" name="email" id="login"
+            <input type="text" class="form-control mb-3" name="login" id="login"
                 placeholder="Username or Email address">
             <input type="password" name="password" class="form-control mb-3" id="Password" placeholder="Password">
             <button class="btn btn-primary d-block mx-auto" type="submit">Login</button>
