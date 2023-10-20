7da460b2234a1e25948faec59f80b0687863e94d
MoatasemGamal <moatasemco@gmail.com>
2023-10-20 15:10:52 +0300

    extends main.layout in all views

--- src/App/views/profile.view.php
+++ src/App/views/profile.view.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Back</title>
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-</head>
-
-<body>
-    <div class="text-bg-dark d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
-        <img src="/assets/images/elephant.png" width=" 250px" alt="">
-        <h1>Profile View</h1>
-    </div>
-    <script src="/assets/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+@extends('main')
+@section('title', 'profile')
+@section('content')
+<img src="/assets/images/elephant.png" width=" 250px" alt="">
+<h1>Profile View</h1>
+@endSection
