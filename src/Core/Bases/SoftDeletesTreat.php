ef949b5367a98928a3c7b693d9e4e8935910c7d6
MoatasemGamal <moatasemco@gmail.com>
2023-11-03 09:45:23 +0200

    add restore method to restore the soft deleted obj

--- src/Core/Bases/SoftDeletesTreat.php
+++ src/Core/Bases/SoftDeletesTreat.php
@@ -5,4 +5,11 @@ namespace Core\Bases;
 trait SoftDeletesTreat
 {
     protected static string $DELETED_AT = 'deleted_at';
+
+    public function restore(): BaseModel
+    {
+        $this->{static::$DELETED_AT} = null;
+        $this->save();
+        return $this;
+    }
 }
