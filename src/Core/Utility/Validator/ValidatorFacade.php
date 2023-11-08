50a892531e0c6d0b1df8f71e79da053c8a4cc846
MoatasemGamal <moatasemco@gmail.com>
2023-11-08 12:13:24 +0200

    implement __constructor take default input method

--- src/Core/Utility/Validator/ValidatorFacade.php
+++ src/Core/Utility/Validator/ValidatorFacade.php
@@ -4,5 +4,15 @@ namespace Core\Utility\Validator;
 
 class ValidatorFacade
 {
-
+    private $method;
+    private $items = [];
+    public $errors = [];
+    public function __construct(int $method = INPUT_POST)
+    {
+        $this->method = match ($method) {
+            INPUT_POST => INPUT_POST,
+            INPUT_GET => INPUT_GET,
+            default => INPUT_POST
+        };
+    }
 }
