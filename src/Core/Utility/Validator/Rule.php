5cf5e55637f2d5e6c1003fae5ca27c13ba8eb37f
MoatasemGamal <moatasemco@gmail.com>
2023-11-08 12:53:13 +0200

    implement Rule class
    -- check if $name of the rule exist in ValidatorFacade class
    -- it will be clear after implementing rules methods

--- src/Core/Utility/Validator/Rule.php
+++ src/Core/Utility/Validator/Rule.php
@@ -4,5 +4,15 @@ namespace Core\Utility\Validator;
 
 class Rule
 {
+    public string $name;
+    public array $params;
+    public string $message;
+    public function __construct(string $name, array $params = [], string $message = "sorry, not valid")
+    {
+        $availableRules = get_class_methods(ValidatorFacade::class);
+        $this->name = in_array($name, $availableRules) ? $name : "required";
+        $this->params = $params;
+        $this->message = $message;
+    }
 
 }
