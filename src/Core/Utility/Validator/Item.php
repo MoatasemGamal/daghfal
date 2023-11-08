3b3f95de853c67466f0c6d574bc886f7ca591563
MoatasemGamal <moatasemco@gmail.com>
2023-11-08 12:25:55 +0200

    implement constructor and main attributes
    -- item is an input or variable
    -- input can be GET or POST

--- src/Core/Utility/Validator/Item.php
+++ src/Core/Utility/Validator/Item.php
@@ -4,5 +4,43 @@ namespace Core\Utility\Validator;
 
 class Item
 {
-
+    /**
+     * is item variable?
+     * false meaning this item is an input
+     * @var bool
+     */
+    public bool $isVar = false;
+    /**
+     * name of the input or variable
+     * @var string
+     */
+    public string $name;
+    /**
+     * value of the variable, if this item is variable not (GET, POST) input
+     * @var mixed
+     */
+    public mixed $value;
+    /**
+     * request method if item is input not variable (INPUT_POST, INPUT_GET)
+     * @var int
+     */
+    public int $method;
+    /**
+     * rules applied to this item
+     * @var \Core\Utility\Validator\Rule[]
+     */
+    public array $rules = [];
+    public function __construct(string $name, mixed $value = null, $method = INPUT_POST)
+    {
+        $this->name = $name;
+        if (!is_null($value)) {
+            $this->isVar = true;
+            $this->value = $value;
+        }
+        $this->method = match ($method) {
+            INPUT_POST => INPUT_POST,
+            INPUT_GET => INPUT_GET,
+            default => INPUT_POST
+        };
+    }
 }
